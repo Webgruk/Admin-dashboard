f973a739f25e937f8713b8f6e83f6b2dd330eae5
immanuel <immanuelajimah@gmail.com>
2023-08-19 17:38:29 +0100

    created a breakdown page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ import Geograhpy from './scenes/geograhpy/index.jsx'
 import Overview from './scenes/overview/index.jsx'
 import Daily from './scenes/daily/index.jsx'
 import Monthly from './scenes/monthly/index.jsx'
+import Breakdown from './scenes/breakdown/index.jsx'
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -37,6 +38,7 @@ function App() {
         <Route path="/overview" element={<Overview />} />
         <Route path="/daily" element={<Daily />} />
         <Route path="/monthly" element={<Monthly />} />
+        <Route path="/breakdown" element={<Breakdown />} />
       </Route>,
     ),
   )
